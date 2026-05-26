be3d09952873197a025b0f178bda67223b196e98
Senthil455 <senthilrajasen637@gmail.com>
2026-05-26 18:38:54 +0530

    fix: next.config.mjs issue fixed

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
   output: "standalone",
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.join(__dirname, "src");
+    return config;
+  },
 };
 
 export default nextConfig;
